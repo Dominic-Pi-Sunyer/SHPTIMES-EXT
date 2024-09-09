2cb3ba6a720df65b8473d5441561f77f92d0bb8f
Fires170 <149727367+Dominic-Pi-Sunyer@users.noreply.github.com>
2024-09-09 14:15:38 -0700

    Added a calculator

--- SHPTIMES EXT/index.js
+++ SHPTIMES EXT/index.js
@@ -202,3 +202,20 @@ function getCookie(name) {
     }
     return null;
 }
+// Caculcaotr
+
+function clearScreen() {
+    document.getElementById("result").value = "";
+}
+
+// This function displays the values
+function display(value) {
+    document.getElementById("result").value += value;
+}
+
+// This function evaluates the expression and returns the result
+function calculate() {
+    var p = document.getElementById("result").value;
+    var q = eval(p);
+    document.getElementById("result").value = q;
+}
